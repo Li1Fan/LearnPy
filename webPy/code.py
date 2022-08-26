e900653033b72e13b6c7061ec46912490f348c3c
frz <fangruizhi@star-net.cn>
2022-08-26 17:19:50 +0800

    webPy增加乘法、表单、索引

--- webPy/code.py
+++ webPy/code.py
@@ -7,17 +7,22 @@ urls = (
     '/favicon.ico', 'ico',
     # '/hello/(.*)', 'sayHello',
     '/hello', 'sayHello',
-    '/add', 'add'
+    '/add', 'add',
+    '/99', 'multiply',
+    '/form', 'form'
 )
 # 使用render到模板目录中去查找相应模板
 render = web.template.render('templates/')
 # 也可以使用frender直接找到指定模板
 hello = web.template.frender('templates/hello.html')
+multi = web.template.frender('templates/multi.html')
+index_page = web.template.frender('templates/index.html')
+form_page = web.template.frender('templates/form.html')
 
 
 class index:
     def GET(self):
-        return "Hello, world!"
+        return index_page()
 
 
 class ico:
@@ -44,7 +49,23 @@ class add:
         raise web.seeother('/hello')
 
 
+class multiply:
+    def GET(self):
+        return multi()
+
+
+class form:
+    def GET(self):
+        value = web.input()
+        print("value:{}".format(value))
+        return form_page()
+
+    def POST(self):
+        data = web.input()
+        print(data)
+        raise web.seeother('/form?data={}'.format(data.fruit.encode('utf-8').decode('latin1')))
+
+
 if __name__ == "__main__":
     app = web.application(urls, globals())
     app.run()
-    # print(''.upper())
