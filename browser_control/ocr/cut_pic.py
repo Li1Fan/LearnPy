5968dbee2212c26b9430427ddf7756dddc571db6
frz <fangruizhi@star-net.cn>
2022-12-02 17:49:23 +0800

    1.修复切割

--- browser_control/ocr/cut_pic.py
+++ browser_control/ocr/cut_pic.py
@@ -1,31 +1,46 @@
-# -*- coding: utf-8 -*-
-import cv2
+import os
+from PIL import Image
 
 
-def split_image(path, row_num, col_num):
-    img = cv2.imread(path)
-    size = img.shape[0:2]
-    w = size[1]
-    h = size[0]
-    # 每行的高度和每列的宽度
-    row_height = h // row_num
-    col_width = w // row_num
-    for i in range(row_num):
-        for j in range(col_num):
-            # 保存切割好的图片的路径，记得要填上后缀，以及名字要处理一下
-            save_path = path.split('.')[0] + '_' + str((i + 1) * (j + 1)) + '.jpg'
-            # print(save_path)
-            row_start = j * col_width
-            row_end = (j + 1) * col_width
-            col_start = i * row_height
-            col_end = (i + 1) * row_height
-            # print(row_start, row_end, col_start, col_end)
-            child_img = img[col_start:col_end, row_start:row_end]
-            cv2.imwrite(save_path, child_img)
+def splitimage(src, rownum, colnum, dstpath):
+    img = Image.open(src)
+    w, h = img.size
+    if rownum <= h and colnum <= w:
+        print('Original image info: %sx%s, %s, %s' % (w, h, img.format, img.mode))
+        print('开始处理图片切割, 请稍候...')
+
+        s = os.path.split(src)
+        if dstpath == '':
+            dstpath = s[0]
+        fn = s[1].split('.')
+        basename = fn[0]
+        ext = fn[-1]
+
+        num = 0
+        rowheight = h // rownum
+        colwidth = w // colnum
+        for r in range(rownum):
+            for c in range(colnum):
+                box = (c * colwidth, r * rowheight, (c + 1) * colwidth, (r + 1) * rowheight)
+                img.crop(box).save(os.path.join(dstpath, basename + '_' + str(num) + '.' + ext), ext)
+                num = num + 1
+
+        print('图片切割完毕，共生成 %s 张小图片。' % num)
+    else:
+        print('不合法的行列切割参数！')
 
 
-if __name__ == '__main__':
-    src_path = 'code.png'
-    row = 1
-    col = 2
-    split_image(src_path, row, col, src_path.split('.')[0])
+src = input('请输入图片文件路径：')
+if os.path.isfile(src):
+    dstpath = input('请输入图片输出目录（不输入路径则表示使用源图片所在目录）：')
+    if (dstpath == '') or os.path.exists(dstpath):
+        row = int(input('请输入切割行数：'))
+        col = int(input('请输入切割列数：'))
+        if row > 0 and col > 0:
+            splitimage(src, row, col, dstpath)
+        else:
+            print('无效的行列切割参数！')
+    else:
+        print('图片输出目录 %s 不存在！' % dstpath)
+else:
+    print('图片文件 %s 不存在！' % src)
