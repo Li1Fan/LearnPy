903108a045731d13ade546e386a9e18a8427f5b8
fangruizhi <fangruizhi@star-net.cn>
2023-05-26 17:57:38 +0800

    flask项目，增加登录、注销实现

--- 11-网站搭建/flask_login_system/app.py
+++ 11-网站搭建/flask_login_system/app.py
@@ -3,11 +3,14 @@ import json
 from flask import Flask, redirect, url_for, request, session, render_template
 
 app = Flask(__name__)
+# 设置密钥
 app.secret_key = 'secret_key'
+# 设置session的过期时间
+app.permanent_session_lifetime = 60 * 10
 
+# 设置用户信息
 user_info = {'admin': '123',
-             'guest': '123'
-             }
+             'guest': '123'}
 
 
 @app.route('/')
@@ -18,6 +21,20 @@ def index():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
+        print(f'form: {request.form}')
+        print(f'cookies: {request.cookies}')  # cookies存储在客户端，session存储在服务端，session的ID存储在客户端的cookies中
+        # 判断用户的登录状态
+        username = session.get('username')
+        if username:
+            return redirect(url_for('main'))
+        return redirect(url_for('login'))
+    return render_template('login.html')
+
+
+@app.route('/login/verify', methods=['GET', 'POST'])
+def login_verify():
+    if request.method == 'POST':
+        # 请求数据
         data = json.loads(request.data.decode('utf-8'))
         print(f'data: {data}')
         if data['username'] not in user_info.keys():
@@ -29,16 +46,25 @@ def login():
         # 设置会话中的用户信息
         session['username'] = data['username']
         session['password'] = data['password']
+
         response = {'status': 0, 'msg': '登录成功'}
         return json.dumps(response)
-    return render_template('login.html')
 
 
 @app.route('/logout')
 def logout():
     # 删除会话中的用户信息
-    session.pop('user')
+    session.pop('username')
     return redirect(url_for('index'))
+
+
+@app.route('/main')
+def main():
+    # 判断用户的登录状态
+    username = session.get('username')
+    if username:
+        return '登录用户是:' + username + '<br>' + "<b><a href = '/logout'>点击这里注销</a></b>"
+    return redirect(url_for('login'))
 
 
 if __name__ == '__main__':
--- 11-网站搭建/flask_login_system/static/query.js
+++ 11-网站搭建/flask_login_system/static/query.js
@@ -5,7 +5,7 @@ function checkPassword() {
 
   // 在这里你可以通过 Ajax 请求将用户名和密码发送给后端进行验证
   // 这里为了简单起见，直接在前端进行密码验证
-  if(username == "" || password == ""){
+  if (username == "" || password == "") {
     alert("用户名或密码不能为空");
     return false;
   }
@@ -13,28 +13,32 @@ function checkPassword() {
     username: username,
     password: password
   };
-  fetch('/login', {
+  fetch('/login/verify', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
     },
     body: JSON.stringify(data)
   })
-  .then(function(response) {
-    if (response.ok) {
-      // 处理响应结果
-      return response.json().then(function(data) {
-        if (data.status == 0) {
-          alert("登录成功！");
-        } else {
-          alert("用户名或密码错误！");
-        }
-      });
-    } else {
-      throw new Error('请求失败：' + response.status);
-    }
-  })
-  .catch(function(error) {
-    alert(error.message);
-  });
+    .then(function (response) {
+      if (response.ok) {
+        // 处理响应结果
+        return response.json().then(function (data) {
+          if (data.status == 0) {
+            alert("登录成功！");
+            submitForm();
+          } else {
+            alert("用户名或密码错误！");
+          }
+        });
+      } else {
+        throw new Error('请求失败：' + response.status);
+      }
+    })
+    .catch(function (error) {
+      alert(error.message);
+    });
+}
+function submitForm() {
+  document.getElementById("loginForm").submit();
 }
